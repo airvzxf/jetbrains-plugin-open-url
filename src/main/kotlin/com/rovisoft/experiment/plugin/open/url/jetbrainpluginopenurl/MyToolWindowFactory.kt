e3b625b90c463d5d17531fcd70c4cfa1b3c91618
Israel Roldan <israel.alberto.rv@gmail.com>
2025-06-27 14:07:03 -0600

    feat: add functionality for buttons to open in a web browser.

--- src/main/kotlin/com/rovisoft/experiment/plugin/open/url/jetbrainpluginopenurl/MyToolWindowFactory.kt
+++ src/main/kotlin/com/rovisoft/experiment/plugin/open/url/jetbrainpluginopenurl/MyToolWindowFactory.kt
@@ -4,31 +4,90 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.ide.BrowserUtil
+import com.intellij.ui.components.JBTextField
 import com.intellij.ui.content.ContentFactory
+import java.awt.Desktop
 import javax.swing.BoxLayout
 import javax.swing.JButton
 import javax.swing.JLabel
 import javax.swing.JPanel
 import javax.swing.JSeparator
+import java.net.URI
 
 class MyToolWindowFactory : ToolWindowFactory {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         val contentFactory = ContentFactory.getInstance()
         val panel = JPanel()
         panel.layout = BoxLayout(panel, BoxLayout.Y_AXIS)
-        panel.add(JLabel("This plugin tests the `BrowserUtil.browse(uri)`."))
+        panel.add(JLabel("This plugin tests open an URL in a web browser."))
         panel.add(JSeparator())
 
-        val button = JButton("Show Message")
-        button.addActionListener {
+        // URL input field
+        val urlPanel = JPanel()
+        urlPanel.layout = BoxLayout(urlPanel, BoxLayout.X_AXIS)
+        urlPanel.add(JLabel("URL: "))
+        val urlTextField = JBTextField("https://accounts.google.com/o/oauth2/auth?client_id=…")
+        urlPanel.add(urlTextField)
+        panel.add(urlPanel)
+
+        // "Show Message" button
+        val showMessageButton = JButton("Show Message")
+        showMessageButton.addActionListener {
+            val url = urlTextField.text
             Messages.showMessageDialog(
                 project,
-                "Hi, from my first Plugin in JetBrains!",
+                "Hi! It is my first plugin in JetBrains.\n\nURL: $url",
                 "Hello World",
                 Messages.getInformationIcon()
             )
         }
-        panel.add(button)
+        panel.add(showMessageButton)
+
+        // "Open in Browser" button
+        val openBrowserButton = JButton("Open in browser | BrowserUtil.browse(url)")
+        openBrowserButton.addActionListener {
+            val url = urlTextField.text
+            if (url.isNullOrBlank()) {
+                Messages.showMessageDialog(
+                    project,
+                    "Please enter a URL.",
+                    "Error",
+                    Messages.getErrorIcon()
+                )
+            } else {
+                BrowserUtil.browse(url)
+            }
+        }
+        panel.add(openBrowserButton)
+
+        // "Open in old Browser" button
+        val openBrowserOldButton = JButton("Open in browser | Desktop.getDesktop().browse(uri)")
+        openBrowserOldButton.addActionListener {
+            val url = urlTextField.text
+            if (url.isNullOrBlank()) {
+                Messages.showMessageDialog(
+                    project,
+                    "Please enter a URL.",
+                    "Error",
+                    Messages.getErrorIcon()
+                )
+            } else {
+                try {
+                    val uri = URI(url)
+                    Desktop.getDesktop().browse(uri)
+                } catch (e: Exception) {
+                    Messages.showMessageDialog(
+                        project,
+                        "Invalid URL: ${e.message}",
+                        "Error",
+                        Messages.getErrorIcon()
+                    )
+                }
+
+            }
+        }
+        panel.add(openBrowserOldButton)
 
         val content = contentFactory.createContent(panel, "", false)
         toolWindow.contentManager.addContent(content)
